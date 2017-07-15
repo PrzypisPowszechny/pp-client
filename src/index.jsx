cd2cc0f659e6ee2ab63841120a6eed8f6a9b4715
kaglowka <krz.glowka@gmail.com>
2017-07-15 17:00:50 +0200

    Set up annotator plugin (2.0.0-alpha.3)

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import annotator from 'annotator';
 
 export default class App extends React.Component {
 
@@ -23,9 +24,20 @@ export default class App extends React.Component {
     }
 }
 
-console.log("Przypis script working!")
+var annotation_prefix = 'http://localhost:8080';
 
+console.log("Przypis script working!");
+
+// will run only in browser environment
 if (typeof window !== 'undefined') {
+
+    var annotator_app = new annotator.App();
+    annotator_app.include(annotator.ui.main);
+    // annotator_app.include(annotator.storage.debug);
+    annotator_app.include(annotator.storage.http, {prefix: annotation_prefix});
+    annotator_app.start();
+
+
     var app = document.createElement("app");
     document.body.appendChild(app);
 
@@ -35,4 +47,3 @@ if (typeof window !== 'undefined') {
     );
 
 }
-
--- webpack.config.js
+++ webpack.config.js
@@ -5,26 +5,42 @@ var BUILD_DIR = path.resolve(__dirname, 'src/public');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-  entry: "./src/index.jsx",
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module : {
-      loaders: [
-          {
-              test: /\.jsx?/,
-              include: APP_DIR,
-              exclude: /node_modules/,
-              loaders: [{
-                  loader: 'babel-loader',
-                  query: {
-                      presets: ['es2015', 'react']
-                  }
-              }
-              ]
-          }]
-  }
+    entry: "./src/index.jsx",
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+
+            {
+                test: /\.jsx?/,
+                include: APP_DIR,
+                exclude: /node_modules/,
+                loaders: [
+                    {
+                        loader: 'babel-loader',
+                        query: {
+                            presets: ['es2015', 'react']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000
+                }
+            }
+
+
+        ]
+    }
 };
 
 module.exports = config;
