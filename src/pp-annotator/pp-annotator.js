75313c80fffc382aaaf28bc764018addd091aeda
Łukasz Godziejewski <lgodziejewski@gmail.com>
2017-11-06 23:05:09 +0100

    pp-annotator.js: rework style to es6, import cleanup

--- src/pp-annotator/pp-annotator.js
+++ src/pp-annotator/pp-annotator.js
@@ -1,26 +1,24 @@
-"use strict";
+import { PrzypisEditor } from './editor';
+import { PrzypisAdder } from './adder';
 
-var util = require('annotator').util;
+import { util, ui as PPUI } from 'annotator';
+
+const { editor, highlighter, textselector, viewer } = PPUI;
+const { gettext: _t } = util;
 
 
-var editor = require('annotator').ui.editor;
-var highlighter = require('annotator').ui.highlighter;
-var textselector = require('annotator').ui.textselector;
-var viewer = require('annotator').ui.viewer;
+/* TODO cleanup
+const annotationFactory = require('annotator').ui.annotationFactory;
+const addPermissionsCheckboxes = require('annotator').ui.addPermissionsCheckboxes;
+const injectDynamicStyle = require('annotator').ui.injectDynamicStyle;
+const removeDynamicStyle = require('annotator').ui.removeDynamicStyle;
+*/
 
-var _t = util.gettext;
-
-// var annotationFactory = require('annotator').ui.annotationFactory;
-// var addPermissionsCheckboxes = require('annotator').ui.addPermissionsCheckboxes;
-// var injectDynamicStyle = require('annotator').ui.injectDynamicStyle;
-// var removeDynamicStyle = require('annotator').ui.removeDynamicStyle;
-
-var PrzypisEditor = require('./editor').PrzypisEditor;
-var PrzypisAdder = require('./adder').PrzypisAdder;
-
-// trim strips whitespace from either end of a string.
-//
-// This usually exists in native code, but not in IE8.
+/**
+ * trim strips whitespace from either end of a string.
+ *
+ * This usually exists in native code, but not in IE8.
+ */
 function trim(s) {
     if (typeof String.prototype.trim === 'function') {
         return String.prototype.trim.call(s);
@@ -30,36 +28,39 @@ function trim(s) {
 }
 
 
-// annotationFactory returns a function that can be used to construct an
-// annotation from a list of selected ranges.
+/**
+ * annotationFactory returns a function that can be used to construct an
+ * annotation from a list of selected ranges.
+ */
 function annotationFactory(contextEl, ignoreSelector) {
     return function (ranges) {
-        var text = [],
+        const text = [],
             serializedRanges = [];
 
-        for (var i = 0, len = ranges.length; i < len; i++) {
-            var r = ranges[i];
+        for (let i = 0, len = ranges.length; i < len; i++) {
+            const r = ranges[i];
             text.push(trim(r.text()));
             serializedRanges.push(r.serialize(contextEl, ignoreSelector));
         }
 
         return {
             quote: text.join(' / '),
-            ranges: serializedRanges
+            ranges: serializedRanges,
         };
     };
 }
 
-
-// maxZIndex returns the maximum z-index of all elements in the provided set.
+/**
+ * maxZIndex returns the maximum z-index of all elements in the provided set.
+ */
 function maxZIndex(elements) {
-    var max = -1;
-    for (var i = 0, len = elements.length; i < len; i++) {
-        var $el = util.$(elements[i]);
+    let max = -1;
+    for (let i = 0, len = elements.length; i < len; i++) {
+        const $el = util.$(elements[i]);
         if ($el.css('position') !== 'static') {
             // Use parseFloat since we may get scientific notation for large
             // values.
-            var zIndex = parseFloat($el.css('z-index'));
+            const zIndex = parseFloat($el.css('z-index'));
             if (zIndex > max) {
                 max = zIndex;
             }
@@ -68,27 +69,28 @@ function maxZIndex(elements) {
     return max;
 }
 
-
-// Helper function to inject CSS into the page that ensures Annotator elements
-// are displayed with the highest z-index.
+/**
+ * Helper function to inject CSS into the page that ensures Annotator elements
+ * are displayed with the highest z-index.
+ */
 function injectDynamicStyle() {
     util.$('#annotator-dynamic-style').remove();
 
-    var sel = '*' +
-              ':not(annotator-adder)' +
-              ':not(annotator-outer)' +
-              ':not(annotator-notice)' +
-              ':not(annotator-filter)';
+    const sel = '*' +
+        ':not(annotator-adder)' +
+        ':not(annotator-outer)' +
+        ':not(annotator-notice)' +
+        ':not(annotator-filter)';
 
     // use the maximum z-index in the page
-    var max = maxZIndex(util.$(global.document.body).find(sel).get());
+    let max = maxZIndex(util.$(global.document.body).find(sel).get());
 
     // but don't go smaller than 1010, because this isn't bulletproof --
     // dynamic elements in the page (notifications, dialogs, etc.) may well
     // have high z-indices that we can't catch using the above method.
     max = Math.max(max, 1000);
 
-    var rules = [
+    const rules = [
         ".annotator-adder, .annotator-outer, .annotator-notice {",
         "  z-index: " + (max + 20) + ";",
         "}",
@@ -103,14 +105,17 @@ function injectDynamicStyle() {
         .appendTo('head');
 }
 
-
-// Helper function to remove dynamic stylesheets
+/**
+ * Helper function to remove dynamic stylesheets
+ */
 function removeDynamicStyle() {
     util.$('#annotator-dynamic-style').remove();
 }
 
-// pp annotator ui module (almost unchanged annotator.ui.main)
-function ui(options) {
+/**
+ * pp annotator ui module (almost unchanged annotator.ui.main)
+ */
+export function ui(options) {
     if (typeof options === 'undefined' || options === null) {
         options = {};
     }
@@ -121,21 +126,21 @@ function ui(options) {
 
     // Local helpers
     console.log(typeof (annotationFactory));
-    var makeAnnotation = annotationFactory(options.element, '.annotator-hl');
+    const makeAnnotation = annotationFactory(options.element, '.annotator-hl');
 
     // Object to hold local state
-    var s = {
+    const s = {
         interactionPoint: null
     };
 
     function start(app) {
-        var ident = app.registry.getUtility('identityPolicy');
-        var authz = app.registry.getUtility('authorizationPolicy');
+        const ident = app.registry.getUtility('identityPolicy');
+        const authz = app.registry.getUtility('authorizationPolicy');
 
         s.adder = new PrzypisAdder({
             beginAnnotationCreate: function (annotation) {
                 s.editor.load(annotation, s.interactionPoint)
-                    .then(function(annotation) {
+                    .then(function (annotation) {
                         app.annotations.create(annotation);
                     });
 
@@ -157,7 +162,7 @@ function ui(options) {
         s.textselector = new textselector.TextSelector(options.element, {
             onSelection: function (ranges, event) {
                 if (ranges.length > 0) {
-                    var annotation = makeAnnotation(ranges);
+                    const annotation = makeAnnotation(ranges);
                     s.interactionPoint = util.mousePosition(event);
                     s.adder.load(annotation, s.interactionPoint);
                 } else {
@@ -169,11 +174,10 @@ function ui(options) {
         s.viewer = new viewer.Viewer({
             onEdit: function (annotation) {
                 // Copy the interaction point from the shown viewer:
-                s.interactionPoint = util.$(s.viewer.element)
-                                         .css(['top', 'left']);
+                s.interactionPoint = util.$(s.viewer.element).css(['top', 'left']);
 
                 s.editor.load(annotation, s.interactionPoint)
-                    .then(function(annotation) {
+                    .then(function (annotation) {
                         app.annotations.update(annotation);
                     });
             },
@@ -206,23 +210,33 @@ function ui(options) {
             removeDynamicStyle();
         },
 
-        annotationsLoaded: function (anns) { s.highlighter.drawAll(anns); },
-        annotationCreated: function (ann) { s.highlighter.draw(ann); },
-        annotationDeleted: function (ann) { s.highlighter.undraw(ann); },
-        annotationUpdated: function (ann) { s.highlighter.redraw(ann); },
+        annotationsLoaded: function (anns) {
+            s.highlighter.drawAll(anns);
+        },
+        annotationCreated: function (ann) {
+            s.highlighter.draw(ann);
+        },
+        annotationDeleted: function (ann) {
+            s.highlighter.undraw(ann);
+        },
+        annotationUpdated: function (ann) {
+            s.highlighter.redraw(ann);
+        },
 
-        // beforeAnnotationCreated: function (annotation) {
-        //     // Editor#load returns a promise that is resolved if editing
-        //     // completes, and rejected if editing is cancelled. We return it
-        //     // here to "stall" the annotation process until the editing is
-        //     // done.
-        //     return s.editor.load(annotation, s.interactionPoint);
-        // },
-        //
-        // beforeAnnotationUpdated: function (annotation) {
-        //     return s.editor.load(annotation, s.interactionPoint);
-        // }
+        /**
+         Editor#load returns a promise that is resolved if editing
+         completes, and rejected if editing is cancelled. We return it
+         here to "stall" the annotation process until the editing is
+         done.
+         */
+        /*
+        beforeAnnotationCreated: function (annotation) {
+            return s.editor.load(annotation, s.interactionPoint);
+        },
+
+        beforeAnnotationUpdated: function (annotation) {
+            return s.editor.load(annotation, s.interactionPoint);
+        }
+        */
     };
 }
-
-exports.ui = ui
