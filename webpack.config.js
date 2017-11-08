f4d74023c52170f9193fe61b92e889839ee03dfd
Łukasz Godziejewski <lgodziejewski@gmail.com>
2017-11-08 01:23:08 +0100

    Move public folder one level up, added UglifyJS plugin to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,52 @@
+const webpack = require('webpack');
 const path = require('path');
 
-const BUILD_DIR = path.resolve(__dirname, 'src/public');
+const BUILD_DIR = path.resolve(__dirname, 'public');
 const APP_DIR = path.resolve(__dirname, 'src');
 
 const config = {
-    entry: "./src/index.jsx",
-    output: {
-        path: BUILD_DIR,
-        filename: 'bundle.js'
-    },
-    module: {
+  entry: "./src/index.jsx",
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        exclude: /node_modules/,
         loaders: [
-            {
-                test: /\.jsx?/,
-                include: APP_DIR,
-                exclude: /node_modules/,
-                loaders: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['es2015', 'react'],
-                            plugins: [
-                                'transform-object-rest-spread',
-                                'transform-class-properties'
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader"
-            },
-            {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000
-                }
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'react'],
+              plugins: [
+                'transform-object-rest-spread',
+                'transform-class-properties'
+              ]
             }
-
-
+          }
         ]
-    }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+    }),
+  ],
 };
 
 module.exports = config;
