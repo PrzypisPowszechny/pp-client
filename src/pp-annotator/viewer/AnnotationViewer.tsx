519a8f9ea7b12909ce13ad4e28e1fb4fcb9c51de
Szymon Rutkowski <szymon@szymonrutkowski.pl>
2017-12-09 22:45:51 +0100

    icons can be seen in viewer, putting some variables in styles

--- src/pp-annotator/viewer/AnnotationViewer.tsx
+++ src/pp-annotator/viewer/AnnotationViewer.tsx
@@ -50,30 +50,31 @@ export default class AnnotationViewer extends React.Component<
 
     return (
             <div className="pp-annotation pp-item">
+		<div className="pp-view-head-bar">
+                <div className="pp-view-comment-priority">
+                    {priority}
+                </div>
+
+		<div className="pp-view-comment-date">
+			01.01.1999
+		</div>
+
                 <div className={"pp-controls " + (this.state.initialView ? "pp-visible" : "")}>
-		<div className="ui icon basic buttons">
+		<div className="ui icon basic mini buttons">
                     <button type="button"
                             title="Edit"
-                            className="pp-edit ui basic button"
+                            className="pp-edit ui button"
                             onClick={(e) => this.props.callbacks.onEdit(e, this.props.annotation)}>
 		    <i className="edit icon"></i>
 		    </button>
                     <button type="button"
                             title="Delete"
-                            className="pp-delete ui basic button"
+                            className="pp-delete ui button"
                             onClick={(e) => this.props.callbacks.onDelete(e, this.props.annotation)}>
 		    <i className="trash icon"></i>
 		    </button>
                 </div>
                 </div>
-
-		<div className="pp-view-head-bar">
-                <div className="pp-view-comment-priority">
-                    {priority}
-                </div>
-		<div className="pp-view-comment-date">
-			01.01.1999
-		</div>
 		</div>
 
                 <div className="pp-view-comment">
