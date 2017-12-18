d4b601fafc8c7e338d0a3bf9e28b5ac1d4f1c2b4
Łukasz Godziejewski <lgodziejewski@gmail.com>
2017-12-18 23:06:00 +0100

    DebugStorage: add logger mechanism, fix memory bloat (unnecessary array creation) in delete method, remove commented code

--- src/pp-annotator/api/DebugStorage.ts
+++ src/pp-annotator/api/DebugStorage.ts
@@ -12,31 +12,39 @@ const generateId = (() => {
 export default class DebugStorage implements IStorage, IModule {
   annotations: IAnnotation[] = [];
 
+  trace(action: string, data: any) {
+    console.debug(`DebugStorage: invoking action ${action} on data: `, data);
+  }
+
   create(annotation: IAnnotation): IAnnotation {
     if (annotation.id === undefined) {
       annotation.id = generateId();
     }
+    this.trace('create', annotation);
+
     this.annotations = this.annotations.concat(annotation);
     return annotation;
   }
 
   update(annotation: IAnnotation): IAnnotation {
+    this.trace('update', annotation);
+
     this.delete(annotation);
-    // this.annotations = this.annotations.concat(annotation);
     return this.create(annotation);
   }
 
   delete(annotation: IAnnotation): IAnnotation {
-    console.log(this.annotations);
-    const deleted = this.annotations[annotation.id];
-    this.annotations = [
-      ...this.annotations.filter(ann => ann.id !== annotation.id),
-    ];
+    this.trace('delete', annotation);
+
+    const deleted = this.query(annotation.id);
+    this.annotations = this.annotations.filter(ann => ann.id !== annotation.id);
 
     return deleted;
   }
 
-  query(id: number): IAnnotation {
+  query(id: string | number): IAnnotation {
+    this.trace('query', id);
+
     return this.annotations.find(annotation => annotation.id === id) as IAnnotation;
   }
 
