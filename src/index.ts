baaaee3423883e4b59c0de5aa3a9c67d2a4aaf41
kaglowka <krz.glowka@gmail.com>
2018-01-20 19:45:04 +0100

    Refactored index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,57 +12,72 @@ import 'css/editor.scss';
 import IPPSettings from './PPSettings.interface';
 declare const PP_SETTINGS: IPPSettings;
 
-// Optional file-input annotations
+// Optional external hardcoded annotations
 import input_annotations from '../config/annotations.json';
 
 console.log('Przypis script working!');
 
-const isBrowser = typeof window !== 'undefined';
-if (isBrowser) {
+function startApp() {
   const annotatorApp = new App();
   annotatorApp.include(PPUI);
   annotatorApp.include(DebugStorage);
   annotatorApp.start();
+  return annotatorApp;
+}
+
+function attachMockViewer() {
+  // IMPORTANT: very implementation dependent; whenever ViewerWidget changes break it, consider simply dropping it
+  const viewer = new ViewerWidget({});
+  viewer.attach();
+
+  const annotation = new AnnotationViewModel({});
+  // mock data to display
+  annotation.comment = 'Testowy komentarz '.repeat(10);
+  annotation.referenceLinkTitle = 'Strona organizacji XYZ '.repeat(3);
+  const position = {
+    top: 500,
+    left: 200,
+  };
+
+  viewer.load([annotation], position);
+  // make the window visible
+  viewer.element.removeClass(ViewerWidget.classes.hide);
+  // turn off the mouseleave listeners so that windows does not disappear on cursor action
+  viewer.element.off('mouseleave.' + ViewerWidget.nameSpace);
+}
+
+function attachAnnotationsFromFile(annotatorApp: App) {
+  // Reassign annotations' ids starting from 1000
+  // so they don't collide with the ids of the annotations created through the application
+  let id = 1000;
+  for (const annotation of input_annotations) {
+    if (annotation.url === window.location.href) {
+      annotation.id = id;
+      annotatorApp.annotations.create(annotation);
+      id += 1;
+    }
+  }
+  console.log('Loaded ' + input_annotations.length + ' annotations from the annotation file.');
+}
+
+const isBrowser = typeof window !== 'undefined';
+if (isBrowser) {
+  // Start the app
+  const annotatorApp = startApp();
 
   /*
-    MOCK FUNCTIONALITY for development purposes
-    Enabled in `config/app-settings.js`
+   MOCK FUNCTIONALITY for development purposes
+   Enabled in `config/app-settings.js`
    */
   if (PP_SETTINGS.MOCK_VIEWER) {
-    // IMPORTANT: very implementation dependent; whenever ViewerWidget changes break it, consider simply dropping it
-    const viewer = new ViewerWidget({});
-    viewer.attach();
-
-    const annotation = new AnnotationViewModel({});
-    // mock data to display
-    annotation.comment = 'Testowy komentarz '.repeat(10);
-    annotation.referenceLinkTitle = 'Strona organizacji XYZ '.repeat(3);
-    const position = {
-          top: 500,
-          left: 200,
-        };
-
-    viewer.load([annotation], position);
-    // make the window visible
-    viewer.element.removeClass(ViewerWidget.classes.hide);
-    // turn off the mouseleave listeners so that windows does not disappear on cursor action
-    viewer.element.off('mouseleave.' + ViewerWidget.nameSpace);
+    attachMockViewer();
   }
 
   /*
-    Read annotations for UX research purposes from a file (only if the storage is local)
+   Read hardcoded annotations for UX research purposes from a file (only if the storage is local)
    */
   const storage = annotatorApp.registry.getUtility('storage');
   if (PP_SETTINGS.READ_ANNOTATIONS_FROM_FILE && storage instanceof DebugStorage) {
-      let id = 1000;
-      for (const annotation of input_annotations) {
-        if (annotation.url === window.location.href) {
-          // Reassign annotation's id so it doesn't collide with the ids of the annotations added through application
-          annotation.id = id;
-          annotatorApp.annotations.create(annotation);
-          id += 1;
-        }
-      }
-      console.log('Loaded ' + input_annotations.length + ' annotations from the annotation file.');
+    attachAnnotationsFromFile(annotatorApp);
   }
 }
