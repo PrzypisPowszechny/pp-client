f5ff54087d88ac9a95e224946bff10319dba29e8
Łukasz Godziejewski <lgodziejewski@gmail.com>
2018-04-16 22:31:38 +0200

    Add Redux Devtools config

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,9 +4,15 @@ import rootReducer from './reducer';
 import promise from 'redux-promise';
 import { createLogger } from 'redux-logger';
 
+// TS override
+declare global {
+  interface Window { __REDUX_DEVTOOLS_EXTENSION__: any; }
+}
+
 const logger = createLogger();
 const store = createStore(
   rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk, promise, logger),
 );
 
