22ced533e3ac6a4f112777ac81c0def7365b18b2
kaglowka <krz.glowka@gmail.com>
2018-04-19 02:05:57 +0200

    Remove `inverted` from reducer

--- src/store/annotations/reducers.ts
+++ src/store/annotations/reducers.ts
@@ -1,10 +1,6 @@
 
 const initialState = [];
 
-function isInverted(x: number, y: number) {
-  return [false, false];
-}
-
 export default function editor(state = initialState, action) {
   return state;
 }
--- src/store/editor/reducers.ts
+++ src/store/editor/reducers.ts
@@ -7,7 +7,6 @@ import AnnotationViewModel from "../../models/AnnotationViewModel";
 export interface IEditorState {
   visible: boolean;
   location: {x: number; y: number};
-  inverted: {x: boolean; y: boolean};
   annotation: AnnotationViewModel;
 }
 
@@ -17,33 +16,19 @@ const initialState = {
     x: 0,
     y: 0,
   },
-  inverted: {
-    x: false,
-    y: false,
-  },
   annotationId: null,
-
 };
-
-function isInverted(x: number, y: number) {
-  return [false, false];
-}
 
 export default function editor(state = initialState, action) {
   switch (action.type) {
     case EDITOR_VISIBLE_CHANGE:
       const location = action.payload.location || state.location;
-      const inverted = isInverted(location.x, location.y);
       return Object.assign({}, state, {
           visible: action.payload.visible,
           location: {
             x: location.x,
             y: location.y,
-          },
-          inverted: {
-            x: inverted[0],
-            y: inverted[1],
-          },
+          }
       });
     case EDITOR_NEW_ANNOTATION:
       return Object.assign({}, state, {
