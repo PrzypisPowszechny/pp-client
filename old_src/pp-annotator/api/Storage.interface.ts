0a3f299e40531fe17fa2eaea15305ef5b717fb94
kaglowka <krz.glowka@gmail.com>
2018-06-04 23:19:30 +0200

    Removed old_src

--- old_src/pp-annotator/api/Storage.interface.ts
+++
@@ -1,8 +0,0 @@
-import {IAnnotationAPIModel} from '../annotation/IAnnotationAPIModel';
-
-export default interface IStorage {
-  create(annotation: IAnnotationAPIModel): IAnnotationAPIModel;
-  update(annotation: IAnnotationAPIModel): IAnnotationAPIModel;
-  delete(annotation: IAnnotationAPIModel): IAnnotationAPIModel;
-  query(id?: number): IAnnotationAPIModel[];
-}
--- old_src/pp-annotator/modules/Module.interface.ts
+++
@@ -1,7 +0,0 @@
-export default interface IModule {
-  start?(...args: any[]): any;
-  destroy?(...args: any[]): any;
-  configure?(...args: any[]): any;
-
-  [x: string]: any;
-}
