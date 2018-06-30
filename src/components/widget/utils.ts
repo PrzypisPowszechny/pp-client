860a74dee31df37c10651f2488db9835c9413cbe
Tomek Glowka <tk.glowka@gmail.com>
2018-06-30 04:18:54 +0200

    Transparane offset around widget, resolves #134

--- src/components/widget/utils.ts
+++ src/components/widget/utils.ts
@@ -3,6 +3,8 @@ export function isInverted(widget, window) {
     // console.log('orientation');
     // TODO replace annotator jQuery with pure js
     const win = $(window);
+    // This number must be synchronized with $transparent-offset from Widget.scss
+    const transparentOffset = 50;
 
     if (widget) {
       const $widget = $(widget);
@@ -17,12 +19,12 @@ export function isInverted(widget, window) {
       };
       // console.log(this.props);
       let invertedX = false;
-      if ((current.right - viewport.right) > 0) {
+      if ((current.right - viewport.right) > transparentOffset) {
         invertedX = true;
       }
 
       let invertedY = false;
-      if ((current.top - viewport.top) < 0) {
+      if ((current.top - viewport.top) < transparentOffset) {
         invertedY = true;
       }
       return {
