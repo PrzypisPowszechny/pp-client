918a59c3aa2b02218170d2bf888695304cfbf9e8
kaglowka <krz.glowka@gmail.com>
2018-07-06 21:07:47 +0200

    Disabled extension mode overrides (negates) annotation mode

--- src/store/appModes/selectors.ts
+++ src/store/appModes/selectors.ts
@@ -7,14 +7,17 @@ export const selectModeForCurrentPage = createSelector<ITabState, any, any>(
   (appModes) => {
     // Standardize the URL by disregarding stuff that does not identify a page like URL parameters etc.
     const currentStandardizedUrl = standardizeUrlForPageSettings(window.location.href);
-
+    console.log(appModes.disabledPages.includes(currentStandardizedUrl));
     return {
-    // Page highlights are disabled when
-    //  the extension is disabled in general or when it is disabled for this particular page
+      // Page highlights are disabled when
+      //  the extension is disabled in general or when it is disabled for this particular page
       arePageHighlightsDisabled: appModes.isExtensionDisabled
       || appModes.disabledPages.indexOf(currentStandardizedUrl) !== -1,
-      // Annotation mode is on when the current URL
-      isAnnotationMode: appModes.annotationModePages.indexOf(currentStandardizedUrl) !== -1,
+      // Annotation mode is on when
+      // - the current URL is included in annotationModePages
+      // - the extension is not globally disabled
+      isAnnotationMode: appModes.annotationModePages.includes(currentStandardizedUrl)
+      && !appModes.isExtensionDisabled,
     };
   },
 );
