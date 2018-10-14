e48282e2277de2cb19c3823c90d6ee0d20307913
Tomek Glowka <tk.glowka@gmail.com>
2018-10-14 21:21:46 +0200

    Enhance shallow rendering of App

--- src/content-scripts/components/App.test.tsx
+++ src/content-scripts/components/App.test.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import Enzyme from 'enzyme';
 import AdapterReact16 from 'enzyme-adapter-react-16';
 import configureStore from 'redux-mock-store';
-
+import ConnectedApp from './App';
 
 jest.mock('common/pp-ga/ga', () => null);
 jest.mock('common/chrome-storage', () => null);
-
-import App from './App';
 
 Enzyme.configure({ adapter: new AdapterReact16() });
 const middlewares = [];
@@ -16,15 +13,39 @@ const mockStore = configureStore(middlewares);
 
 describe('App component', () => {
   it('shallow renders without crashing', () => {
-    const initialState = {};
+    const initialState = {
+      widgets: {
+        menu: {
+          visible: false,
+        },
+        editor: {
+          visible: false,
+        },
+        viewer: {
+          visible: false,
+        },
+      },
+      appModes: {
+        isExtensionDisabled: false,
+        disabledPages: [],
+      },
+    };
     const store = mockStore(initialState);
 
-    // TODO: currently Connect() wrapper is shallow rendered here and we would like to get to App itself
-    Enzyme.shallow((
-      <Provider store={store}>
-        <App/>
-      </Provider>
-    ));
+    // Shallow render of the connect
+    let wrapper = Enzyme.shallow(
+      <ConnectedApp/>,
+      { context: { store } },
+    );
+    expect(wrapper.find('App').length).toBeTruthy();
+    const app = wrapper.getElement();
+
+    // Shallow render of connect output - App instance
+    wrapper = Enzyme.shallow(
+      app,
+      { context: { store } },
+    );
+    expect(wrapper.exists()).toBeTruthy();
   });
 
   it('mounts without crashing', () => {
@@ -63,11 +84,10 @@ describe('App component', () => {
 
     const store = mockStore(initialState);
 
-    Enzyme.mount((
-      <Provider store={store}>
-        <App/>
-      </Provider>
-    ));
+    Enzyme.mount(
+      <ConnectedApp/>,
+      { context: { store } },
+    );
 
   });
 });
