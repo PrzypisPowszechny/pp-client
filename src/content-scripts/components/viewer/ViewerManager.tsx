74625eb674c788cf793148f945edf54c61b0fdf7
Krzysztof <krz.glowka@gmail.com>
2018-11-10 15:36:26 +0100

    Revert temporary viewer development setup

--- src/content-scripts/components/viewer/ViewerManager.tsx
+++ src/content-scripts/components/viewer/ViewerManager.tsx
@@ -147,7 +147,6 @@ export default class ViewerManager extends React.Component<Partial<IViewerManage
   }
 
   render() {
-    // return this.props.visible && <Viewer/>;
-    return  <Viewer/>;
+    return this.props.visible && <Viewer/>;
   }
 }
