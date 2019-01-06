cc24d594c72bae0d0515ea1b189c7a95b17f5bdf
Krzysztof <krz.glowka@gmail.com>
2019-01-06 18:11:22 +0100

    Add basic annotation summary logic

--- src/popup/components/annotationSummary/AnnotationSummary.tsx
+++ src/popup/components/annotationSummary/AnnotationSummary.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { loadAnnotationLocationData, PopupAnnotationLocationData } from '../../messages';
+import {
+  AnnotationAPIModel,
+  AnnotationPPCategories,
+  annotationPPCategoriesLabels,
+} from 'common/api/annotations';
+import _countBy from 'lodash/countBy';
 
 export interface IAnnotationSummaryProps {
   onFullViewClick: (Event) => void;
@@ -28,9 +34,38 @@ export default class AnnotationSummary extends React.Component<Partial<IAnnotati
     });
   }
 
-  render() {
+  renderSummary() {
+    const annotations: AnnotationAPIModel[] = this.state.annotationLocationData.located;
+    const counts = {
+      [AnnotationPPCategories.ADDITIONAL_INFO]: 0,
+      [AnnotationPPCategories.CLARIFICATION]: 0,
+      [AnnotationPPCategories.ERROR]: 0,
+      ..._countBy(annotations.map(annotation => annotation.attributes.ppCategory)),
+    };
+    // TODO ignore annotations with zero count
+    // TODO style
     return (
-      <div>{this.state.isLoading ? 'loading' : 'loaded'}</div>
+      <ul>
+        <li>{annotationPPCategoriesLabels.ADDITIONAL_INFO} : {counts[AnnotationPPCategories.ADDITIONAL_INFO]}</li>
+        <li>{annotationPPCategoriesLabels.CLARIFICATION} : {counts[AnnotationPPCategories.CLARIFICATION]}</li>
+        <li>{annotationPPCategoriesLabels.ERROR} : {counts[AnnotationPPCategories.ERROR]}</li>
+      </ul>
     );
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <div className="annotation-summary">
+          loading...
+        </div>);
+    } else {
+      return (
+        <div className="annotation-summary">
+          <button onClick={this.props.onFullViewClick}>See more</button>
+          {this.renderSummary()}
+        </div>
+      );
+    }
   }
 }
