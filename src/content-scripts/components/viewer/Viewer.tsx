9f2e4769e92fcff241ec61041b94dba70d45423f
Krzysztof <krz.glowka@gmail.com>
2019-01-13 21:03:41 +0100

    Replace semantic ui modal with our custom one in DeleteAnnotationModal

--- src/content-scripts/components/viewer/Viewer.tsx
+++ src/content-scripts/components/viewer/Viewer.tsx
@@ -9,7 +9,7 @@ import styles from './Viewer.scss';
 import { selectViewerState } from 'content-scripts/store/widgets/selectors';
 import { PPScopeClass, PPViewerHoverContainerClass, PPViewerIndirectChildClass } from 'content-scripts/settings';
 import { setMouseOverViewer } from 'content-scripts/store/widgets/actions';
-import DeleteAnnotationModal from './DeleteAnnotationModal';
+import _DeleteAnnotationModal, { default as DeleteAnnotationModal } from './DeleteAnnotationModal';
 
 interface IViewerProps {
   locationX: number;
